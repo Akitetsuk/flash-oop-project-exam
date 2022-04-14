d9eabecb2014d19dae67e4b861c728bbb727f740
Akitetsuk <m.feijoo.1995@gmail.com>
2022-04-14 13:55:59 +0200

    done

--- js/app.js
+++ js/app.js
@@ -4,16 +4,36 @@ let nextId = 3;
 Vue.createApp({
     data() {
         return {
-            notes: [{
-                id: 1,
+            isHidden: false,
+            question : "",
+            answer:"",
+            notes: [
+                {id: 1,
                 question: "¿Cómo hago para iterar un array con Vue?",
-                answer: "Usa la directiva v-for"
-            }, {
-                id: 2,
+                answer: "Usa la directiva v-for" }, 
+                {id: 2,
                 question: "¿Cómo hago para relacionar una variable de estado con el campo <em>value</em>' de input?",
-                answer: "La directiva v-model nos ayudará en este caso"
-
-            }]
+                answer: "La directiva v-model nos ayudará en este caso"}
+            ]
         }
-    }
+    },
+
+    methods: {
+        addTodo() {
+            
+            this.notes.push(
+                {nextId: nextId++,
+                question: this.question,
+                answer: this.answer,
+            });
+
+            this.question = "";
+            this.answer ="";
+
+        },
+
+        removeItem(note) {
+            this.notes = this.notes.filter((t) => t !== note);
+        },
+    },
 }).mount('#app')
